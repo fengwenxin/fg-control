e305f9885bc1a32f8d4a19159168876ae471fab2
fengwenxin <407715370@qq.com>
2020-07-15 14:28:21 +0800

    v0.0.9

--- src/index.js
+++ src/index.js
@@ -1,200 +1,206 @@
-
 /**
- * 流控引擎 v0.0.8
+ * 流控引擎
  */
 class FG {
-  constructor() {
+    constructor() {
 
-    this.ISOK = false;
-    this.CURFORM= null;   // 当前展示的是input_config_code 还是 output_config_code 表单
-    this.OUTFLAG= null;   // 提交标识
-    this.FUNC= null;      // 函数列表
-    // 流程节点
-    this.START = "01"; // 开始
-    this.END   = "02"; // 结束
-    this.DOING = "03"; // 业务类型
-    // 提交方式
-    this.COMMIT_TYPE_DEFAULT = "01"; // 默认提交
-    this.COMMIT_TYPE_DEFINE  = "02"; // 自定义提交
-    this.COMMIT_TYPE_ORDER   = "03"; // 订单提交
-    this.COMMIT_TYPE_LOCAL   = "04"; // 本地提交
+        this.ISOK = false;
+        // this.CURFORM= null;   // 当前展示的是input_config_code 还是 output_config_code 表单
+        this.OUTFLAG= null;   // 提交标识
+        // 流程节点
+        this.START = "01"; // 开始
+        this.END   = "02"; // 结束
+        this.DOING = "03"; // 业务类型
+        // 提交方式
+        this.COMMIT_DEFAULT = "01"; // 默认提交
+        this.COMMIT_DEFINE  = "02"; // 自定义提交
+        this.COMMIT_ORDER   = "03"; // 订单提交
+        this.COMMIT_LOCAL   = "04"; // 本地提交
+        // 返回设置
+        this.CAN_ROLLBACK = "01"; // 可以回退
+        this.CANNOT_ROLLBACK = "02"; // 不可回退
+        // 是否保留数据
+        this.CLEAR_DATA = "01", // 清除数据
+            this.KEEP_DATA = "02",  // 保留数据
 
-    this.user = {};
-    this.platform =  {};
-    this.list =  {};
-    this.func =  {};
-
-    // 流控数据
-    this.flow = {
-      // isOK: false,     // 标识这个流是否可以执行
-      // curForm: null,   // 当前展示的是input_config_code 还是 output_config_code 表单
-      // OutFlag: null,   // 提交标识
-      // func: null,      // 函数列表
-      // flow_code: null, // 流程编码
-      // list: [],        // 流程数据
-    };
-  }
-
-  /**
-   * 初始化成功
-   */
-  init() {
-    console.log('engine init...')
-  }
-
-  getAllData(){
-    return {
-        user:this.user,
-        platform:this.platform,
-        CURFORM:this.CURFORM ,
-        OUTFLAG:this.OUTFLAG ,
-        func:this.func,
-        list:this.list,
+            // 数据
+            this.user = {};
+        this.platform =  {};
+        this.nodes =  {};
+        this.list =  {};
+        this.utils =  {};
+        // 流程当前的执行过程
+        this.process = [];
     }
-  }
 
-  /**
-   * 同步流程数据
-   * @param object
-   */
-  setState(object) {
-    Object.assign(this.flow, object)
-  }
-  settters(key,value){
-    this[key] = value;
-  }
-
-  /**
-   * 检查节点能否执行
-   * @param checkstart
-   * @returns {*}
-   */
-  checkStart(checkstart) {
-    try {
-      const list = this.list;
-      const platform = this.platform;
-      const user = this.user;
-      if (checkstart) {
-        return checkstart && eval(checkstart);
-      }
-      return true;
-    } catch (e) {
-      throw new Error('init checkstart执行出错了')
+    /**
+     * 开始节点
+     * @returns {*}
+     */
+    getStartNode(){
+        if(this.list.length == 0){
+            return {}
+        }
+        return this.list.filter(item => item.type == this.START)[0]
     }
-  }
 
-  /**
-   * 获取流控节点中配置的数据
-   * @param fn
-   * @returns {any}
-   */
-  _getFieldsData(fn) {
-    try {
-      const flow = this.flow;
-      const obj = eval(fn);
-      for (let key in obj) {
-        // console.log(key,(obj[key]))
-        obj[key] = eval(obj[key]);
-      }
-      return obj;
-    } catch (e) {
-      throw new Error('init func[input_config] 执行出错了')
+    /**
+     * 结束节点
+     * @returns {*}
+     */
+    getEndNode(){
+        if(this.list.length == 0){
+            return {}
+        }
+        return this.list.filter(item => item.type == this.END)[0]
     }
-  }
-
-  /**
-   * 过滤数据
-   * @param node_code
-   * @returns {*}
-   * @private
-   */
-  _filter(node_code) {
-    // return this.flow.list.filter(item => item.node_code == node_code);
-    return this.list.filter(item => item.node_code == node_code);
-  }
-
-  /**
-   * 获取流控中配置的数据
-   * @param node_code
-   * @returns {*}
-   */
-  getInputData(node_code) {
-    // const flw = this.flow.list.filter(item => item.node_code == node_code)[0];
-    const flw = this._filter(node_code)[0];
-    const {input_config} = flw;
-    const {func} = this.flow;
-    if (func && input_config) {
-      const fn = func[input_config];
-      return this._getFieldsData(fn);
-    } else {
-      return {}
+    /**
+     * 设置数据
+     * @param object
+     */
+    setData(key,value){
+        this[key] = value;
     }
-  }
 
-  /**
-   * 上一节点
-   * @param return_node
-   * @returns {*}
-   */
-  getPrev(return_node) {
-    if (return_node) {
-      // const prev = this.flow.list.filter(item => item.node_code == return_node);
-      const prev = this._filter(return_node);
-      if (prev instanceof Array && prev.length > 0) {
-        return prev[0]
-      }
+    /**
+     * 检查节点能否执行
+     * @param checkstart
+     * @returns {*}
+     */
+    checkStart(checkstart) {
+        if(checkstart){
+            return this._solveConfigJs(checkstart)
+        }
+        return true;
     }
-  }
 
-  /**
-   * 下一节点
-   * @param next_node
-   * @returns {*}
-   */
-  getNext(next_node) {
-    console.log('next_node', next_node)
-    if (next_node) {
-      // const next = this.flow.list.filter(item => item.node_code == next_node);
-      const next = this._filter(next_node);
-      if (next instanceof Array && next.length > 0) {
-        return next[0];
-      }
+    /**
+     * 处理公共函数js代码
+     * @param JsCode
+     * @returns {any}
+     * @private
+     */
+    solveCommonJS(JsCode){
+        try {
+            return eval(JsCode)
+        }catch (error) {
+            console.log('===commonJs===',JsCode)
+            throw  new Error(error)
+        }
     }
-  }
 
-  /**
-   * 保存节点数据
-   * @param nodeCode
-   * @param value
-   */
-  saveNode(nodeCode, value) {
-    this.flow[nodeCode] = value;
-    console.log('flow', this.flow)
-  }
+    /**
+     * 处理配置数据js代码
+     * @param inputConfigJs
+     * @returns {*}
+     * @private
+     */
+    _solveConfigJs(JsCode){
+        try {
+            const platform = this.platform;
+            const user = this.user;
+            const nodes = this.nodes;
+            const utils = this.utils;
+            console.log('{ platform, user, nodes, utils} ',{ platform, user, nodes, utils} )
+            const resCode = `function _execute(user, platform, nodes, utils){  ${JsCode}  return main(...arguments);}`;
+            // console.log('resCode',resCode)
+            const exeCode = eval("(" + resCode + ")");
+            let rs = exeCode(user, platform, nodes, utils);
+            return rs;
+        } catch (error) {
+            console.log('===inputConfigJs===',JsCode)
+            throw new Error(error)
+        }
+    }
 
-  /**
-   * 默认提交
-   * @param Obj
-   * @returns {Promise<any>}
-   */
-  commitDefault(Obj) {
-    return new Promise((resolve, reject) => {
-      console.log("执行默认的提交", Obj)
-      resolve(Obj)
-    })
-  }
+    /**
+     * 过滤数据
+     * @param node_code
+     * @returns {*}
+     * @private
+     */
+    _filter(node_code) {
+        return this.list.filter(item => item.nodeCode == node_code);
+    }
 
+    /**
+     * 下一节点
+     * @param next_node
+     * @returns {*}
+     */
+    getNext(next_node) {
+        console.log('next_node', next_node)
+        if (next_node) {
+            const next = this._filter(next_node);
+            if (next instanceof Array && next.length > 0) {
+                return next[0];
+            }
+        }
+    }
+
+    /**
+     * 节点数据
+     * @param nodeCode
+     * @returns {*}
+     */
+    getNodeData(nodeCode){
+        return this._filter(nodeCode)[0];
+    }
+
+    /**
+     * 保存节点数据
+     * @param nodeCode
+     * @param value
+     */
+    saveNode(nodeCode, value) {
+        this.nodes[nodeCode] = value;
+        console.log('nodes', JSON.stringify(this.nodes))
+    }
+
+    /**
+     * 获取所有节点数据
+     * @returns {{}|*}
+     */
+    getNodes(){
+        return this.nodes;
+    }
+
+    /**
+     * 节点加入到执行流程
+     * @param nodeCode
+     */
+    pushProcess(nodeCode){
+        if(this.process.includes(nodeCode)){
+            return
+        }
+        this.process.push(nodeCode)
+    }
+
+    /**
+     * 获取执行流程
+     * @returns {Array}
+     */
+    getProcess(){
+        return this.process;
+    }
+
+    /**
+     * 删除最后一个元素
+     */
+    popProcess(){
+        this.process.pop()
+    }
 }
 
 // 返回单例
 let getFG = (function () {
-  let fee
-  return function () {
-    if (!fee) {
-      fee = new FG();
+    let fee
+    return function () {
+        if (!fee) {
+            fee = new FG();
+        }
+        return fee
     }
-    return fee
-  }
 })()
 
 export default getFG;
